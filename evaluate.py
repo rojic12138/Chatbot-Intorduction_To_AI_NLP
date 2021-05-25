143910392bcd421b7b9ccd22dd2af228881effb6
罗皓rojic <43370437+rojic12138@users.noreply.github.com>
2021-05-25 20:20:27 +0800

    Add files via upload

---
+++ evaluate.py
@@ -0,0 +1,125 @@
+#Written by LH 
+import torch
+import os
+import torch.nn as nn
+'''
+贪心解码算法 :
+    每次选择概率最高的词，作为下一个时刻的输入，直到遇到EOS或者达到最大长度
+输入序列(input_seq)，其shape是(input_seq length, 1)， 输入长度input_length和最大输出长度max_length
+过程：
+1) 把输入传给Encoder，得到所有时刻的输出和最后一个时刻的隐状态。 
+2) 把Encoder最后时刻的隐状态作为Decoder的初始状态。 
+3) Decoder的第一输入初始化为SOS。 
+4) 定义保存解码结果的tensor 
+5) 循环直到最大解码长度 
+    a) 把当前输入传入Decoder 
+    b) 得到概率最大的词以及概率 
+    c) 把这个词和概率保存下来 
+    d) 把当前输出的词作为下一个时刻的输入 
+6) 返回所有的词和概率
+#也可以使用Beam-Search算法，也就是每个时刻保留概率最高的Top K个结果，然后下一个时刻尝试把这K个结果输入(当然需要能恢复RNN的状态)，然后再从中选择概率最高的K个。
+'''
+
+class GreedySearchDecoder(nn.Module):
+    def __init__(self,encoder,decoder):
+        super(GreedySearchDecoder,self).__init__()
+        self.encoder=encoder
+        self.decoder=decoder
+    def forward(self,input_seq,input_length,max_length):
+        #encoder的forward计算 
+        encoder_outputs, encoder_hidden = self.encoder(input_seq, input_length)
+        #将encoder最后时刻的隐状态作为docoder的初始值
+        decoder_hidden=encoder_hidden[:decoder.n_layers]
+        #Decoder的初始输入是SOS
+        decoder_input=torch.ones(1,1,device=device,dtype=torch.long)*SOS_token
+        #保存解码结果
+        all_tokens=torch.zeros([0],device=device,dtype=toch.long)
+        all_scores=torch.zeros([0],device=device)
+        
+        for _ in range(max_length):
+            #decoder forward一步
+            decoder_output, decoder_hidden = self.decoder(decoder_input, decoder_hidden, 
+								encoder_outputs)
+            # decoder_outputs是(batch=1, vob_size)
+            #使用max返回概率最大的词和得分
+            decoder_scores, decoder_input = torch.max(decoder_output, dim=1)
+            # 把解码结果保存到all_tokens和all_scores里
+            all_tokens = torch.cat((all_tokens, decoder_input), dim=0)
+            all_scores = torch.cat((all_scores, decoder_scores), dim=0)
+            # decoder_input是当前时刻输出的词的ID，这是个一维的向量，因为max会减少一维。
+            # 但是decoder要求有一个batch维度，因此用unsqueeze增加batch维度。
+            decoder_input = torch.unsqueeze(decoder_input, 0)
+        # 返回所有的词和得分。
+        return all_tokens, all_scores
+    
+    
+'''
+测试从终端输入一个句子，看bot的回复
+过程：
+1)分词 变成ID
+2)传入解码器得到ID 
+3)转换成词
+'''
+def evaluate(encoder, decoder, searcher, voc, sentence, max_length=MAX_LENGTH):
+    #句子->ID
+    indexes_batch=[indexesFromSentence(voc,sentence)]
+    lengths=torch.tensor([len(indexes)for indexes in indexes_batch])
+    #转置 
+    input_batch = torch.LongTensor(indexes_batch).transpose(0, 1)
+    #放到GPU中
+    input_batch = input_batch.to(device)
+    lengths = lengths.to(device)
+    #用GreedySearchDecoder的实例searcher来解码
+    tokens, scores = searcher(input_batch, lengths, max_length)
+    #ID->词
+    decoded_words = [voc.index2word[token.item()] for token in tokens]
+    return decoded_words
+
+
+ '''
+ evaluateInput是chatbot的用户接口
+ 过程：
+ 1)输入一个句子
+ 2)用evaluate生成回 去
+ 3)掉EOS之后的内容
+ 4)重复对话，直到输入q/quit
+ '''
+    
+def evaluateInput(encoder, decoder, searcher, voc):
+    input_sentence = ''
+    while(1):
+        try:
+            s
+            input_sentence = input('请输入> ')
+            #输入q/quit，结束程序
+            if input_sentence == 'q' or input_sentence == 'quit': break
+            #句子归一化
+            input_sentence = normalizeString(input_sentence)
+            #生成答句
+            output_words = evaluate(encoder, decoder, searcher, voc, input_sentence)
+            #去掉EOS后面的内容
+            words = []
+            for word in output_words:
+                if word == 'EOS':
+                    break
+                elif word != 'PAD':
+                    words.append(word)
+            print('尬聊Bot:', ' '.join(words))
+        #错误处理
+        except KeyError:
+            print("Error: Encountered unknown word.")
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
---
+++ train.py
@@ -0,0 +1,165 @@
+#Written by LH 
+import torch
+import os
+import torch.nn as nn
+import numpy as np
+'''
+训练分为八步：
+1) 把整个batch的输入传入encoder 
+2) 把decoder的输入设置为特殊的，初始隐状态设置为encoder最后时刻的隐状态 
+3) decoder每次处理一个时刻的forward计算 
+4) 如果是teacher forcing，把上个时刻的"正确的"词作为当前输入，否则用上一个时刻的输出作为当前时刻的输入 
+5) 计算loss 
+6) 反向计算梯度 
+7) 对梯度进行裁剪 
+8) 更新模型(包括encoder和decoder)参数
+
+问题：teacher forcing用到的"当前正确答案"哪里来的
+'''
+def train(input_variable, lengths,target_variable,mask,max_target_len,encoder,decoder,embedding,
+          encoder_optimizer,decoder_optimizer,vatch_size,clip,max_length=MAX_LENGTH):
+    #梯度清空
+    encoder_optimizer.zero_grad()
+    decoder_optimizer.zero_grad()
+    #设置device到GPU中
+    input_variable=input_variable.to(device)
+    lengths=lengths.to(device)
+    target_variable=target_variable.to(device)
+    mask=mask.to(device)
+    #初始化变量
+    loss=0
+    print_lossed=[]
+    n_totals=0
+    
+    #encoder的forward计算
+    encoder_outputs,encoder_hidden=encoder(input_variable,lengths)
+    
+    #decoder的初始输入是SOS，需要构造(1,batch)的输入，表示第一个时刻的batch个输入
+    decoder_input=torch.LongTensor([[SOS_token for _ in range(batch_size)]])
+    decoder_input=decoder_input.to(device)
+    decoder_hidden=encoder_hidden[:decoder.n_layers]
+    
+    #确定是否采用teacher forcing
+    use_teacher_forcing=True if random.random()<teacher_forcing_ratio else False
+    
+    
+    #一次处理一个时刻
+    if use_teacher_forcing:
+        for t in range(max_target_len):
+            decoder_output,decoder_hidden=decoder(decoder_input,decoder_hidden,encoder_outputs)
+            #下一个时刻的输入是当前正确答案
+            decoder_input=target_variable[t].view(1,-1)
+            #累计loss
+            mask_loss,nTotal=maskNLLLoss(decoder_output,target_variable[t],mask[t])
+            loss+=mask_loss
+            print_losses.append(mask_loss.item()*nTotal)
+            n_totals+=nTotal
+    else:
+        for t in range(max_target_len):
+            decoder_output,decoder_hidden=decoder(decoder_input,decoder_hidden,encoder_outputs)
+            #下一个时刻的输入是当前模型预测概率最高的值
+            _,topi=decoder_output.topk(1)
+            decoder_input=torch.LongTensor([[topi[i][0] for i in range(batch_size)]])
+            decoder_input=decoder_input.to(device)
+            #累计loss
+            mask_loss,nTotal=maskNLLLoss(decoder_output,target_variable[t],mask[t])
+            loss+=mask_loss
+            print_losses.append(mask_loss.item()*nTotal)
+            n_totals+=nTotal
+    
+    #反向传播
+    loss.backward()
+    
+    #对encoder和decoder进行梯度裁剪
+    _=torch.nn.utils.clip_grad_norm_(encoder.parameters(),clip)
+    _=torch.nn.utils.clip_grad_norm_(decoder.parameters(),clip)
+    
+    #更新
+    encoder_optimizer.step()
+    decoder_optimizer.step()
+    
+    return sum(print_lossed)/n_totals;
+    
+
+#trainIters进行n_iterations次minibatch的训练
+
+
+def trainIters(model_name,voc ,pairs,encoder,decoder,encoder_optimizer,decoder_optimizer,embedding,
+               encoder_n_layers,decoder_n_layers,save_dir,n_iteration,batch_size,print_every,
+               save_every,clip,corpus_name,loadFilename):
+    #随机选择n_iteration个batch的数据(pair)
+    training_batches=[batch2TrainData(voc,[random.choice(pairs) for _ in range(batch_size)]) for _ in range(n_iteration)]
+    #初始化
+    print('初始化中...')
+    start_iteration=1
+    print_loss=0
+    if loadFilename:
+        start_iteration=checkpoint['iteration']+1
+    
+    #训练
+    print('训练中...')
+    for iteration in range(start_iteration,n_iteration+1):
+        training_batch=training_batched[iteration-1]
+        input_variable,lengths,target_vatiable,mask,max_target_len=training_batch
+        
+        #训练一个batch
+        loss=train(input_variable, lengths, target_variable, mask, max_target_len, encoder,
+                     decoder, embedding, encoder_optimizer, decoder_optimizer, batch_size, clip)
+        print_loss += loss
+        
+        #打印进度
+        if iteration % print_every == 0:
+            print_loss_avg = print_loss / print_every
+            print("迭代次数: {}; 完成百分比: {:.1f}%; 平均损失: {:.4f}"
+			.format(iteration, iteration / n_iteration * 100, print_loss_avg))
+            print_loss = 0
+            
+        #保存checkpoint
+        if (iteration % save_every == 0):
+            directory = os.path.join(save_dir, model_name, corpus_name, '{}-{}_{}'
+                                     .format(encoder_n_layers, decoder_n_layers, hidden_size))
+            if not os.path.exists(directory):
+                os.makedirs(directory)
+            torch.save({
+                'iteration': iteration,
+                'en': encoder.state_dict(),
+                'de': decoder.state_dict(),
+                'en_opt': encoder_optimizer.state_dict(),
+                'de_opt': decoder_optimizer.state_dict(),
+                'loss': loss,
+                'voc_dict': voc.__dict__,
+                'embedding': embedding.state_dict()
+            }, os.path.join(directory, '{}_{}.tar'.format(iteration, 'checkpoint')))
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
